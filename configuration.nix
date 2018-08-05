dabe25422c1e5fde06814a378f24dd26d33de6f0
nikki93 <s.nikhilesh@gmail.com>
2018-08-04 18:43:51 -0700

    use 'firefox' instead of 'surf' + 'dmenu'

--- configuration.nix
+++ configuration.nix
@@ -52,7 +52,17 @@
     scrot feh
 
     #((import <nixos-unstable> {}).luakit.overrideAttrs ({ buildInputs, ... }: { buildInputs = buildInputs ++ [ glib-networking ]; }))
-    surf dmenu
+
+    #(surf.overrideAttrs ({ buildInputs, ... }: {
+    #  buildInputs = buildInputs ++ [
+    #    gst_all_1.gstreamer gst_all_1.gst-plugins-base
+    #    gst_all_1.gst-plugins-good gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly
+    #    gst_all_1.gst-libav
+    #  ];
+    #}))
+    #dmenu
+
+    firefox
 
     git
     ripgrep
