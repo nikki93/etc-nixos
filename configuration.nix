695445626cee504c1ad93b2f4eb5e95f4dc325ce
nikki93 <s.nikhilesh@gmail.com>
2018-08-07 16:18:17 -0700

    remove commented-out firewall config

--- configuration.nix
+++ configuration.nix
@@ -66,12 +66,6 @@
   # Bash
   programs.bash.enableCompletion = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # Sound
   sound.enable = true;
   hardware.pulseaudio.enable = true;
